75b8c72625fcf1e7e38fbb1cf16e4e4a5621b580
slice <yoonasy@qq.com>
2017-09-29 01:45:41 +0800

    'create'

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+* @Author: slice
+* @Date:   2017-09-29 01:44:10
+* @Last Modified by:   slice
+* @Last Modified time: 2017-09-29 01:44:42
+*/
+exports.sayHello=function(){
+　　return "Hello,Mr.Z";
+};
